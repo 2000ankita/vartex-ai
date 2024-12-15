45dded820b775fa5c1896400b7487cc70a3268e8
Ankita Vakil <ankitasvakil@gmail.com>
2024-12-15 13:58:49 +0530

    train_pipeline.py9

--- pipelines/compile_pipeline.py
+++ pipelines/compile_pipeline.py
@@ -1,6 +1,13 @@
-import kfp
-from kfp.compiler import Compiler
+import sys
+import os
+
+# Add the parent directory of this file to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Now import your pipeline
 from pipelines.train_pipeline import iris_training_pipeline  # Adjust the import path as necessary
+
+from kfp.compiler import Compiler
 
 # Compile the pipeline
 Compiler().compile(iris_training_pipeline, 'pipeline.json')
